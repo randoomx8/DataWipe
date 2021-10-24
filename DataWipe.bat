3d7aab8b15a9df48e4e7af5b3ef233983d094ab0
randoomx8 <randoomx8@gmail.com>
2021-10-25 00:39:17 +0200

    Update DataWipe.bat

--- DataWipe.bat
+++ DataWipe.bat
@@ -6,6 +6,7 @@ echo Welcome to DataWipe. This is the basic edition, for advanced controls pleas
 echo Before continuing, please make sure to close EVERY application and piece of software that can be closed.
 echo Otherwise, they might try to open unused data (the stuff you are trying to delete) or retain already existing cache data.
 echo.
+echo THIS VERSION IS OUT OF DATE AND WILL BE UPDATED SOON (alongside the advanced version), BUT SOME TESTING IS NECESSARY FIRST.
 pause
 
 cls
