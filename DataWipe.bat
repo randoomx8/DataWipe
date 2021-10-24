93cb8247b0a4f5c5aec4d0b147f60b418c2414ce
randoomx8 <randoomx8@gmail.com>
2021-10-25 00:11:09 +0200

    Add files via upload

---
+++ DataWipe.bat
@@ -0,0 +1,16 @@
+@echo off
+Color 1F
+
+cls
+echo Welcome to DataWipe. This is the basic edition, for advanced controls please download the Advanced version.
+echo Before continuing, please make sure to close EVERY application and piece of software that can be closed.
+echo Otherwise, they might try to open unused data (the stuff you are trying to delete) or retain already existing cache data.
+echo.
+pause
+
+cls
+Color 0c
+cipher /w:c:
+echo.
+echo COMPLETED DATA WIPE. HAVE A SAFE DAY.
+pause
